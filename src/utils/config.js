a9bec6bcd9a8653809fa62c7313a7f3e083e8a24
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2021-11-07 09:49:48 +0530

    fix: fixed env config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
-export const API_URI = process.env.API_URI || "http://localhost:5000";
-export const HOST_URI = process.env.HOST_URI || "http://localhost:3000";
+export const API_URI = process.env.REACT_APP_API_URI || "http://localhost:5000";
+export const HOST_URI =
+  process.env.REACT_APP_HOST_URI || "http://localhost:3000";
