c99b880d6977f3549be10b94a16a4b85ba073a4d
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2021-11-20 10:13:39 +0000

    chores: added config

--- src/stats.js
+++ src/stats.js
@@ -1,24 +1,24 @@
 const statsList = [
   {
     id: 1,
-    title: 'Brand Recognition',
+    title: "Brand Recognition",
     subtitle:
-      'Boost your brand recognition with each click. Generic links don’t mean a thing.Branded links help instil confidence in your content.',
-    icon: '/icon-brand-recognition.svg',
+      "Boost your brand recognition with each click. Generic links don’t mean a thing.Branded links help instil confidence in your content.",
+    icon: "/icon-brand-recognition.svg",
   },
   {
     id: 2,
-    title: 'Detailed Records',
+    title: "Detailed Records",
     subtitle:
-      'Gain insights into who is clicking your links.Knowing when and where people engage with your content helps inform better decisions.',
-    icon: '/icon-detailed-records.svg',
+      "Gain insights into who is clicking your links.Knowing when and where people engage with your content helps inform better decisions.",
+    icon: "/icon-detailed-records.svg",
   },
   {
     id: 3,
-    title: 'Fully Customizable',
+    title: "Fully Customizable",
     subtitle:
-      'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
-    icon: '/icon-fully-customizable.svg',
+      "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+    icon: "/icon-fully-customizable.svg",
   },
 ];
 
