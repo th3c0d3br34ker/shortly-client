beba760ce9429f6a06f16f36c27c8e581d3b5015
th3c0d3br34ker <jainamdesai.learn@gmail.com>
2023-04-16 16:41:47 +0530

    updated app

--- src/components/UrlItem.js
+++ src/components/UrlItem.js
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import copy from "copy-to-clipboard";
+import React, { useState } from 'react';
+import copy from 'copy-to-clipboard';
 
-import { deleteShortUrl } from "../api/url";
+import { deleteShortUrl } from '../api/url';
 
 // project imports
-import { Spinner } from "./Icons";
+import { Spinner } from './Icons';
 
 const UrlItem = ({ result, refresh }) => {
-  const { id, short_url, long_url } = result;
-  const [copyText, setCopyText] = useState("Copy");
+  const { id, short_url, original_url } = result;
+  const [copyText, setCopyText] = useState('Copy');
   const [loading, setLoading] = useState(false);
 
   const handleCopy = () => {
     copy(short_url);
-    setCopyText("Copied!");
+    setCopyText('Copied!');
   };
 
   const handleDelete = async () => {
@@ -24,24 +24,24 @@ const UrlItem = ({ result, refresh }) => {
   };
 
   return (
-    <div className="bg-white border flex flex-wrap items-center justify-between my-4 p-4 rounded text-sm">
-      <p className="mb-4 md:mb-0 truncate border-bottom-2">{long_url}</p>
-      <hr className="bg-gray-100 block mb-4 w-full md:hidden border-separate" />
-      <span className="w-full md:w-auto text-blue-400 flex flex-col md:flex-row md:items-center">
+    <div className='bg-white border flex flex-wrap items-center justify-between my-4 p-4 rounded text-sm'>
+      <p className='mb-4 md:mb-0 truncate border-bottom-2'>{original_url}</p>
+      <hr className='bg-gray-100 block mb-4 w-full md:hidden border-separate' />
+      <span className='w-full md:w-auto text-blue-400 flex flex-col md:flex-row md:items-center'>
         {short_url}
 
         <button
           onClick={handleCopy}
           className={`inline-flex items-center px-4 py-2 md:mt-0 md:ml-4 rounded-xl border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:border-blue-700 active:bg-blue-700 transition ease-in-out duration-150 ${
-            copyText === "Copied!" ? "active" : ""
+            copyText === 'Copied!' ? 'active' : ''
           }`}
         >
           {copyText}
         </button>
         <button
           onClick={handleDelete}
-          type="button"
-          className="inline-flex items-center px-4 py-2 md:mt-0 md:ml-4 rounded-xl  border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:border-red-700 active:bg-red-700 transition ease-in-out duration-150"
+          type='button'
+          className='inline-flex items-center px-4 py-2 md:mt-0 md:ml-4 rounded-xl  border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:border-red-700 active:bg-red-700 transition ease-in-out duration-150'
           disabled={loading}
         >
           {loading && <Spinner />}
